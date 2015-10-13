e3bd4050fb542360b3a5783414aa7730a6a16904
hainandao710 <331733316@qq.com>
2015-10-13 12:28:56 +0800

    Update ip.js

--- js/ip.js
+++ js/ip.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){
   $.ajax({
     type : "GET",
-    url : "http://apis.baidu.com/apistore/iplookupservice/iplookup";
+    url : "http://apis.baidu.com/apistore/iplookupservice/iplookup" ,
     headers : { apikey : 4e7ec4ca4892e4fda4a421466dc9ef39 },
     data : " ip = 117.89.35.58 ",
     success : function (data){
